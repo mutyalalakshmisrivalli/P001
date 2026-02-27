584bcbf26e9565b6a48bea55fc67d56273b783e5
mutyalalakshmisrivalli <mutyalalakshmisrivalli@gmail.com>
2026-02-27 11:00:52 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App(){
+  return(
+ <div>
+    <h1>My Info</h1>
+
+    <ul>
+        <li>Name : Mutyala Lakshmi Srivalli</li>
+        <li>Program : B.Tech.-CSE</li>
+        <li>Section : S03</li>
+        <li>Course : Front End Development Frameworks and UI Engineering</li>
+    </ul>
+</div>
+  );
+}
+
+export default App;
